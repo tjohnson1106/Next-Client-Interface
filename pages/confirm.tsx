b8ca55caa706aa24e11628a872b073492ec14b80
Thomas Johnson <tjohnson1106@gmail.com>
2019-02-01 15:25:54 -0500

    created redirect to login

--- pages/confirm.tsx
+++ pages/confirm.tsx
@@ -1,14 +1,38 @@
+// @ts-ignore
 import React, { PureComponent } from "react";
-import { NextContext } from "next";
+import { ApolloContext } from "../interfaces/ApolloContext";
+import {
+  ConfirmUserMutation,
+  ConfirmUserVariables
+} from "../generated/apolloComponents";
+import { confirmUserMutation } from "../graphql/user/mutations/confirmUser";
+import redirect from "../lib/redirect";
 
 class Confirm extends PureComponent {
-  static getInitialProps = ({ query: { token } }: NextContext) => {
-    return { token };
-  };
+  static async getInitialProps({
+    query: { token },
+    apolloClient,
+    ...ctx
+  }: ApolloContext) {
+    if (!token) {
+      return {};
+    }
+
+    await apolloClient.mutate<ConfirmUserMutation, ConfirmUserVariables>({
+      mutation: confirmUserMutation,
+      variables: {
+        token: token as string
+      }
+    });
+
+    redirect(ctx, "/login");
+
+    return {};
+  }
 
   render() {
     console.log(this.props);
-    return <div>Confirm</div>;
+    return "Something went wrong!";
   }
 }
 
