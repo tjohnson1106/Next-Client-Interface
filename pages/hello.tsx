2ff507c4d0a80b75c55eb736c4f7b010cfe08745
Thomas Johnson <tjohnson1106@gmail.com>
2019-02-04 14:04:44 -0500

    created register, login and 'hello' layout routes

--- pages/hello.tsx
+++ pages/hello.tsx
@@ -1,14 +1,17 @@
 import React from "react";
 import { HelloComponent } from "../generated/apolloComponents";
+import Layout from "../components/Layout";
 
 export default () => {
   return (
     // HellComponent -> calls 'hello' query
-    <HelloComponent>
-      {({ data }) => (
-        // render whatever the query returns
-        <div>{data && data.hello ? data.hello : "loading..."}</div>
-      )}
-    </HelloComponent>
+    <Layout title="hello">
+      <HelloComponent>
+        {({ data }) => (
+          // render whatever the query returns
+          <div>{data && data.hello ? data.hello : "loading..."}</div>
+        )}
+      </HelloComponent>
+    </Layout>
   );
 };
