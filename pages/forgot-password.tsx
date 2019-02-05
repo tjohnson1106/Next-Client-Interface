6bf2e3c4a0149f302ddfe04f313433c63a0620c7
Thomas Johnson <tjohnson1106@gmail.com>
2019-02-05 12:40:13 -0500

    created forgot password ui page

---
+++ pages/forgot-password.tsx
@@ -0,0 +1,43 @@
+import React from "react";
+import { Formik, Field } from "formik";
+import Router from "next/router";
+
+import Layout from "../components/Layout";
+import { InputField } from "../components/fields/InputField";
+import { ForgotPasswordComponent } from "../generated/apolloComponents";
+
+export default () => {
+  return (
+    <Layout title="Forgot Password page">
+      {/* mutation */}
+      <ForgotPasswordComponent>
+        {(forgotPassword) => (
+          <Formik
+            onSubmit={async (data) => {
+              const response = await forgotPassword({
+                variables: data
+              });
+              console.log("response", response, "response");
+              Router.push("/check-email");
+            }}
+            initialValues={{
+              email: ""
+            }}
+          >
+            {({ handleSubmit }) => (
+              <form onSubmit={handleSubmit}>
+                <Field
+                  name="email"
+                  placeholder="email"
+                  component={InputField}
+                />
+
+                <button type="submit">send email</button>
+              </form>
+            )}
+          </Formik>
+        )}
+      </ForgotPasswordComponent>
+    </Layout>
+  );
+};
